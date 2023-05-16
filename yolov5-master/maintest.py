1ba5e3f89bbdc8c4aeaef546902f1c9a4f4c9dcf
Alberto F. Merchán <alberto.fernmer@gmail.com>
2023-05-16 11:51:03 +0200

    update

--- yolov5-master/maintest.py
+++ yolov5-master/maintest.py
@@ -7,25 +7,26 @@ import torchvision.datasets
 from torchvision import datasets, transforms
 
 import os
-url_img = 'C:/Users/usuario/Desktop/Hexapod_Caterpie/Dataset/imagenes/1_NanoBug_Azul/1_NanoBug_Azul_17.jpg'
-image = cv2.imread('C:/Users/usuario/Desktop/Hexapod_Caterpie/Dataset/imagenes/1_NanoBug_Azul/1_NanoBug_Azul_17.jpg')
+# url_img = 'C:/Users/usuario/Desktop/Hexapod_Caterpie/Dataset/imagenes/1_NanoBug_Azul/1_NanoBug_Azul_17.jpg'
+# image = cv2.imread('C:/Users/usuario/Desktop/Hexapod_Caterpie/Dataset/imagenes/1_NanoBug_Azul/1_NanoBug_Azul_17.jpg')
+#
+# height = image.shape[0]
+# width = image.shape[1]
 
-height = image.shape[0]
-width = image.shape[1]
 
-
-def generarpuntos():
-    for i in [0.132716, -0.132716]:
-        for j in [0.131687, -0.131687]:
-            image = cv2.circle(image, (int((0.399691+i)*width), int((0.55144+j)*height)), 2, (0, 255, 255), 2)
-
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# def generarpuntos():
+#     for i in [0.132716, -0.132716]:
+#         for j in [0.131687, -0.131687]:
+#             image = cv2.circle(image, (int((0.399691+i)*width), int((0.55144+j)*height)), 2, (0, 255, 255), 2)
+#
+#     cv2.imshow("Image", image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
 def train():
     # os.system(f"python train.py --img 640 --epochs 3 --data data/data.yaml --weights yolov5s.pt")
-    os.system("python train.py --img 640 --batch 16 --epochs 3 --data data.yaml --weights yolov5s.pt --cache")
+    # os.system("python train.py --data data/data.yaml --epochs 3 --weights '' --cfg yolov5s.yaml  --batch-size -1")
+    os.system("python train.py --img 640 --batch 16 --epochs 3 --data data/data.yaml --cfg yolov5s.yaml")
 
 def test():
     for url_img in os.listdir('C:/Users/usuario/Desktop/Hexapod_Caterpie/Dataset/imagenes'):
@@ -55,6 +56,6 @@ if __name__ == "__main__":
     # output = model(images[374:376])
     # print(output)
     # # Not Enough Memory
-    test()
+    train()
 
     # # model()
