500b304fd45fd0465681a35c14e758b5cf21ca88
Alberto F. Merchán <alberto.fernmer@gmail.com>
2023-05-23 16:48:02 +0200

    imagenes

--- Code/Client/yolov5master/maintest.py
+++ Code/Client/yolov5master/maintest.py
@@ -26,7 +26,7 @@ import os
 def train():
     # os.system(f"python train.py --img 640 --epochs 3 --data data/data.yaml --weights yolov5s.pt")
     # os.system("python train.py --data data/data.yaml --epochs 3 --weights '' --cfg yolov5s.yaml  --batch-size -1")
-    os.system("python train.py --img 640 --batch 32 --epochs 10 --data data/data.yaml --cfg yolov5s.yaml")
+    os.system("python train.py --img 640 --batch 32 --epochs 5 --data data/data.yaml --cfg yolov5s.yaml")
 
 def test(ruta):
     # for url_img in os.listdir('/Code/Client/yolov5master/data/test/images'):
@@ -35,5 +35,5 @@ def test(ruta):
     os.system(f'python yolov5master/detect.py --weights ./runs/train/exp8/weights/best.pt --source {ruta_acceder} --data data.yaml --exist-ok --save-txt --conf-thres {0.6}')
 
 if __name__ == "__main__":
-    # train()
-    print()
+    train()
+    # print()
